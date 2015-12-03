58430fbac7762b4e693e6d821dabe557794e4774
kellijohnson <kellifayejohnson@gmail.com>
2015-12-03 15:24:21 -0800

    Add 2015 data

--- alaska_spde_base.R
+++ alaska_spde_base.R
@@ -5,10 +5,10 @@
 ## Author:     Kelli Faye Johnson
 ## Contact:    kellifayejohnson@gmail.com
 ## Date:       2015-01-05
-## Input:      All data was downloaded from 
+## Input:      All data was downloaded from
 ##             http://www.afsc.noaa.gov/RACE/groundfish/survey_data/data.htm
 ##             and placed in dir.data
-## Comments:   
+## Comments:
 ###############################################################################
 ###############################################################################
 
@@ -30,7 +30,7 @@ run.all <- TRUE #logical; save or load(saved) workspace
 
 desired.areas <- c("ai", "goa")
 desired.spp <- "Pacific cod"
-desired.years <- 1990:2014 #2014 = last year of data in the AI
+desired.years <- 1990:2015 #2015 = last year of data in the AI
 
 ###############################################################################
 #### Load packages
@@ -46,10 +46,10 @@ desired.years <- 1990:2014 #2014 = last year of data in the AI
   }
 
   # Load .R files specific for the alaska analysis, located in "lib" folder
-  sapply(dir(file.path(my.base, "lib"), full.names = TRUE), source) 
+  sapply(dir(file.path(my.base, "lib"), full.names = TRUE), source)
   library("r4kfj")
   load_packages(c("igraph", "INLA", "maps", "maptools", "mapproj", "Matrix",
-                  "plyr", "raster", "rpart", "reshape", "reshape2", "rgdal", 
+                  "plyr", "raster", "rpart", "reshape", "reshape2", "rgdal",
                   "rgeos", "sos", "sp", "spdep", "stats4", "TMB", "xtable"))
 
   define_projections()

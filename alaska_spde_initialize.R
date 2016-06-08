49a8c1828f2480631c3dd578f115c4bb28ab54f1
kellijohnson <kellifayejohnson@gmail.com>
2016-06-08 09:08:52 -0700

    Change some spacing

--- alaska_spde_initialize.R
+++ alaska_spde_initialize.R
@@ -31,45 +31,47 @@ desired.years <- 1990:2015 #2015 = last year of data in the AI
 ###############################################################################
 #### Load packages
 ###############################################################################
-    dir.results <- file.path(my.base, "results")
-    dir.data    <- file.path(my.base, "data")
-    dir.create(dir.results, showWarnings = FALSE)
-    dir.create(dir.data, showWarnings = FALSE)
+dir.results <- file.path(my.base, "results")
+dir.data    <- file.path(my.base, "data")
+dir.create(dir.results, showWarnings = FALSE)
+dir.create(dir.data, showWarnings = FALSE)
 
+if (getweb) {
+  # Install personal R package: "r4kfj"
+  devtools::install_github("kellijohnson/r4kfj@master")
+  # Install the stable version of INLA
+  install.packages("INLA", repos = "http://www.math.ntnu.no/inla/R/stable")
+  # Install a needed package for rgeos
+  install.packages("gpclib", repos = "http://cran.fhcrc.org/", type = "source")
+}
+
+# Install TMB
+if (file.exists(file.path("c:", "adcomp"))) {
+  old_wd <- getwd()
+  setwd(file.path("c:", "adcomp"))
   if (getweb) {
-    # Install personal R package: "r4kfj"
-    devtools::install_github("kellijohnson/r4kfj@master")
-    # Install the stable version of INLA
-    install.packages("INLA", repos = "http://www.math.ntnu.no/inla/R/stable")
-    # Install a needed package for rgeos
-    install.packages("gpclib", repos = "http://cran.fhcrc.org/", type = "source")
+    system("git fetch")
+    system("git rebase origin/master")
   }
-  # Install TMB
-  if (file.exists(file.path("c:", "adcomp"))) {
-    old_wd <- getwd()
-    setwd(file.path("c:", "adcomp"))
-    if (getweb) {
-      system("git fetch")
-      system("git rebase origin/master")
-    }
-    remove.packages("TMB")
-    source("install_windows.R")
-    setwd(old_wd)
-    rm(old_wd)
-  } else {
-    stop("TMB is not installed on this computer.")
-  }
-  # Load .R files specific for the alaska analysis, located in "lib" folder
-  ignore <- sapply(dir(file.path(my.base, "lib"), full.names = TRUE), source)
-  library("r4kfj")
-  library(INLA)
-  load_packages(c("gstat", "igraph", "maps", "maptools", "mapproj", "Matrix",
-    "plyr", "RandomFields",
-    "RANN", "raster", "rpart", "reshape", "reshape2", "rgdal",
-    "rgeos", "sos", "sp", "splancs",
-    "spdep", "stats4", "TMB", "xtable"))
+  remove.packages("TMB")
+  source("install_windows.R")
+  setwd(old_wd)
+  rm(old_wd)
+} else {
+  stop("TMB is not installed on this computer.")
+}
 
-  define_projections()
+# Load .R files specific for the alaska analysis, located in "lib" folder
+ignore <- sapply(dir(file.path(my.base, "lib"), full.names = TRUE), source)
+library("r4kfj")
+library(INLA)
+load_packages(c("gstat", "igraph", "maps", "maptools", "mapproj", "Matrix",
+  "plyr", "RandomFields",
+  "RANN", "raster", "rpart", "reshape", "reshape2", "rgdal",
+  "rgeos", "sos", "sp", "splancs",
+  "spdep", "stats4", "TMB", "xtable"))
+
+define_projections()
 
 ###############################################################################
 #### Get data

43889bee80a67f8da90c901c2938b9a4e2c9bf9f
kellijohnson <kellifayejohnson@gmail.com>
2016-06-10 05:35:48 -0700

    Starting to fix analysis code to use JTT's cpp script.

--- alaska_spde_analysis.R
+++ alaska_spde_analysis.R
@@ -10,26 +10,72 @@
 ###############################################################################
 
 ###############################################################################
-#### Create the mesh using the projected data so all measurements are in m
+# Create the mesh using the projected data so all measurements are in m
+# The function inla.mesh.2d() is recommended and will create
+# Constrained Refined Delaunay Triangulation (CRDT) == mesh
+# The boundary will have a variance 2x the variance of the domain
+# Create the SPDE/GMRF model, (kappa^2-Delta)(tau x) = W:
 ###############################################################################
-  ## Create the SPDE/GMRF model, (kappa^2-Delta)(tau x) = W:
-  prdomain <- inla.nonconvex.hull(coordinates(data.all),
-                                  -0.05,
-                                  resolution = c(40, 15))
 
-  mesh <- inla.mesh.2d(loc = coordinates(data.spp),
-                       offset = c(-0.06, -0.03),
-                       cutoff = 100,
-                       max.edge = c(100, 2000),
-                       boundary = prdomain)
+# use a mesh based on a non-convex hull
+# to avoid adding many small triangles outside the domain of interest
+# (more triangles = larger computation times)
+# inla.nonconvex.hull(points, convex, concave, resolution, eps)
+# points = 2D point coordinates (2-column matrix)
+# convex = desired extension radius
+#   Determines the smallest allowed convex curvature radius.
+#   Negative values are fractions of the approximate initial set diameter.
+# concave = minimal concave curvature radius
+#   Default is concave = convex.
+# resolution = internal computation resolution
+#   A warning will be issued when this needs to be increased for
+#   higher accuracy, with the required resolution stated.
+# eps = The polygonal curve simplification tolerance used for simplifying
+#   the resulting boundary curve.
+prdomain <- inla.nonconvex.hull(coordinates(data.all),
+  convex = -0.05, resolution = c(40, 15))
 
-  spde <- inla.spde2.matern(mesh, alpha = 2)
+# inla.mesh.2d(loc = NULL, loc.domain = NULL, offset = NULL,
+#   n = NULL, boundary = NULL, interior = NULL, max.edge,
+#   min.angle = NULL, cutoff = 1e-12, plot.delay = NULL)
+# loc = locations used as initial triangulation nodes
+# loc.domain = a polygon to determine the domain extent
+# max.edge = specifies the maximum allowed triangle edge lengths
+#   in the inner domain and in the outer extension.
+#   A scalar or length two vector, on the SAME SCALE UNIT as
+#   the coordinates.
+# offset = a numeric, or length two vector.
+#   If negative then it is a factor relative to the approx. data diameter.
+#   If positive it is the extension distance on same scale as coords.
+# n = initial number of points on the extended boundary
+# interior = a list of segments to specify interior constraints,
+#   each one of inla.mesh.segment class.
+#   A good mesh needs to have triangles as regular as possible
+#   in size and shape.
+# min.angle = a scalar or length two vector,
+#   to specify the minimum internal angles of the triangles on
+#   the inner domain and on the outer extension.
+#   Values up to 21 guarantee the convergence of the algorithm.
+# cutoff = minimum allowed distance between points.
+#   Points at a closer distance than the supplied value are replaced
+#   by a single vertex to avoid small triangles.
+#   Critical when we have some very close points, either for
+#   point locations or on the domain boundary.
+mesh <- inla.mesh.2d(loc = coordinates(data.all),
+  offset = c(-0.06, -0.03),
+  cutoff = 100,
+  max.edge = c(100, 2000),
+  boundary = prdomain)
+
+spde <- inla.spde2.matern(mesh)
 
 ###############################################################################
 #### Compile tmb file
 ###############################################################################
-  file.remove(paste0(file.path(dir.data, my.tmb), c(".o", ".dll")))
-  compile(file.path(dir.data, paste0(my.tmb, ".cpp")))
+file.remove(paste0(file.path(dir.data, my.tmb), c(".o", ".dll")))
+compile(file.path(dir.data, paste0(my.tmb, ".cpp")))
+dyn.load(dynlib(file.path(dir.data, my.tmb)))
+newtonOption(smartsearch = TRUE)
 
 ###############################################################################
 #### Set up the data
@@ -56,22 +102,21 @@
 ###############################################################################
 #### TMB
 ###############################################################################
-  dyn.load(dynlib(file.path(dir.data, my.tmb)))
-  newtonOption(smartsearch = TRUE)
+# Build inputs
+  X_xp <- matrix(1, ncol = 1, nrow = spde$n)
 
-  X_xp <- matrix(1, ncol = 1, nrow = mesh$n)
-
-  data.spatial = list(
-    n_x = mesh$n,
+  data = list(
+    n_i = NROW(data.all@data)
+    n_x = spde$n,
     n_t = length(desired.years),
     n_p = ncol(X_xp),
 
-    x_s = mesh$idx$loc - 1,
+    x_s = spde$idx$loc - 1,
 
-    c_i = as.vector(data.spp@data$WTCPUE),
+    c_i = as.vector(data.all@data$WTCPUE),
     s_i = mapval[match(station, mapval$s.unique), "map"],
-    t_i = factor(data.spp$YEAR, levels = desired.years,
-      labels = 1:length(desired.years)),
+    t_i = factor(data.all@data$YEAR, levels = desired.years,
+      labels = 1:length(desired.years)) - 1,
 
     X_xp = X_xp,
 
@@ -80,32 +125,30 @@
     G2 = spde$param.inla$M2
     )
 
-  parameters.spatial = list(
+  parameters = list(
     alpha = c(0.0),
     phi = 0.0,
     log_tau_E = 1.0,
     log_tau_O = 1.0,
     log_kappa = 0.0,
     rho = 0.5,
-    Epsilon_input = matrix(rnorm(spde$n.spde * data.spatial$n_t + 1),
-      nrow = spde$n.spde, ncol = data.spatial$n_t + 1),
-    Omega_input = rnorm(spde$n.spde)
+    Epsilon_input = matrix(rnorm(spde$n * data$n_t),
+      nrow = spde$n, ncol = data$n_t),
+    Omega_input = rnorm(spde$n)
     )
 
-  obj <- MakeADFun(data = data.spatial,
-    parameters = parameters.spatial,
-    random = c("Omega_input", "Epsilon_input"),
+  obj <- MakeADFun(data = data,
+    parameters = parameters,
+    random = c("Epsilon_input", "Omega_input"),
     hessian = FALSE,
     DLL = my.tmb)
 
-  obj$fn(obj$par)
-  obj$control <- list(trace = 1, parscale = rep(1, 13), REPORT = 1,
-    reltol = 1e-12, maxit = 300)
-  obj$hessian <- F
   opt <- nlminb(obj$par, obj$fn, obj$gr,
-    lower = c(rep(-20, 2), rep(-10, 6)), #lower par bounds
-    upper = c(rep(20, 2), rep(10, 6)),   #upper par bounds
-    control = list(eval.max = 1e4, iter.max = 1e4))
+    lower = c(rep(-20, 2), rep(-10, 3), -0.999), #lower par bounds
+    upper = c(rep(20, 2), rep(10, 3), 0.999),    #upper par bounds
+    control = list(eval.max = 1e4, iter.max = 1e4, trace = 1))
+  opt[["final_gradient"]] <- obj$gr(opt$par)
+
   # Obtain standard errors
   Report <- obj$report()
   report <- try(sdreport(obj))

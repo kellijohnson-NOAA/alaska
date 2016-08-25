486af2ef822c74a1966a33a887bfb3e4d5acc388
kellijohnson <kellifayejohnson@gmail.com>
2016-08-25 13:58:44 -0700

    Change to lognormal for weights. Also there was an error in the normal code before, now it actually sums the weights for the number of fish thought to be there.

--- lib/Sim_Gompertz_Fn.R
+++ lib/Sim_Gompertz_Fn.R
@@ -1,6 +1,7 @@
 Sim_Gompertz_Fn <- function(n_years, n_stations = 100, phi = NULL,
-  SpatialScale = 0.1, SD_O = 0.5, SD_E = 0.2, SD_extra = 0.1,
-  rho = 0.8, logMeanDens = 1, Loc = NULL) {
+  SpatialScale = 0.1, SD_O = 0.5, SD_E = 1.0, SD_extra = 1.0,
+  rho = 0.5, logMeanDens = 1, Loc = NULL, projection = akCRS,
+  slope = 0.5, weightvals = c(10, 2)) {
 
   # Parameters
   if(is.null(phi)) phi <- rnorm(1, mean = 0, sd = 1)
@@ -43,10 +44,16 @@ Sim_Gompertz_Fn <- function(n_years, n_stations = 100, phi = NULL,
     Tmp <- c(
       "Site" = s,
       "Year" = t,
-      "Simulated_example" = rpois(1, lambda = exp(Theta[s, t] + SD_extra*rnorm(1))))
+      "Simulated_example" = rpois(1, lambda = exp(Theta[s, t] + SD_extra*rnorm(1))),
+      "Simulated_weight" = NA)
     #, 'Lon..DDD.DDDDD.' = Loc[s, 1], 'Lat..DD.DDDDD.' = Loc[s, 2])
     DF <- rbind(DF, Tmp)
   }}
+  DF$Simulated_weight <- sapply(DF$Simulated_example, function(x) {
+    temp <- rlnorm(x, mean = weightvals[1], sd = weightvals[2])
+    temp <- log(temp * exp(weightvals[2]^2 / 2))
+    return(sum(temp))
+  })
 
   DF <- cbind(DF, 'Longitude' = Loc[DF[, 'Site'], 1],
     'Latitude' = Loc[DF[, 'Site'], 2])

b9e76e386cad27f254bef4caa6a51011309a1c48
kellijohnson <kellifayejohnson@gmail.com>
2016-09-03 06:22:25 -0700

    Add ggplot theme

--- alaska_spde_initialize.R
+++ alaska_spde_initialize.R
@@ -71,4 +71,16 @@ load_packages(c("cluster", "fields", "fpc", "ggplot2", "gstat", "igraph",
 
 define_projections()
 
+# Save custom ggplot theme
+my.theme <- theme(plot.background = element_blank(),
+  panel.grid.major = element_blank(),
+  panel.grid.minor = element_blank(),
+  strip.background = element_blank(),
+  panel.background = element_blank(),
+  panel.border = element_rect(colour = "white", fill = NA, size = 1),
+  legend.key = element_rect(colour = "white"),
+  legend.title = element_text(size = 0, face = "bold"),
+  legend.text = element_text(size = 7, face = "bold")
+)
+
 #EndOfFile

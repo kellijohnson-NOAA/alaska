dcb9e0ea327ff8d46516ce7e8ff039bbd4f1927f
kellijohnson <kellifayejohnson@gmail.com>
2016-09-04 07:12:18 -0700

    Update fig_data with comments and new sample size labels
    Fix a few other minor things.

--- alaska_fig_data.R
+++ alaska_fig_data.R
@@ -14,23 +14,30 @@
 ###############################################################################
 png(filename = file.path(dir.results, "fig_data.png"),
   width = my.width[2], height = my.height, res = my.resolution, units = "in")
+# Subset for the species which also subsets for > 0 measurements
 data.plot <- subset(data.all, SID == race.num[1,1] & YEAR %in% desired.years)
 data.plot$year.f <- factor(data.plot$YEAR, levels = desired.years)
 
+# Determine which region the data comes from
 col.year <- rep(NA, length(desired.years))
 for(y in seq_along(desired.years)){
   if(!desired.years[y] %in% data.plot$YEAR) {next}
   temp <- subset(data.plot, YEAR == desired.years[y])@data[1, "survey"]
   col.year[y] <- ifelse(length(grep("ai", temp)) > 0, 1, 2)
 }
-par(mgp = c(1, 0.5, 0), oma = c(0, 0, 0, 0), mar = c(3, 3, 0, 0))
+
+# Create label of the number of positive tows / all tows
+label <- paste(summary(data.plot$year.f),
+  summary(factor(data.all$YEAR, levels = desired.years)), sep = "\n")
+
+# Start the plot
+par(mgp = c(1, 0.5, 0), oma = c(0, 0, 0, 0), mar = c(3, 3, 0.01, 0.01))
 boxplot(log(WTCPUE) ~ year.f,
   data = data.plot, lty = col.year, las = 1, ann = FALSE)
-mtext("year", side = 1, line = 1.5)
+mtext("Year", side = 1, line = 1.5)
 mtext("ln(CPUE)", side = 2, line = 1.5)
-text(seq_along(desired.years), rep(par("yaxp")[1] - 1, length(desired.years)),
-       labels = summary(data.plot$year.f), cex = 0.65)
-text(0.15, par("yaxp")[1] - 1, "n =", cex = 0.65)
+text(seq_along(desired.years), rep(par("yaxp")[1] * 1.35, length(desired.years)),
+       labels = label, cex = 0.65)
 legend("topleft", lty = 1:2, legend = c("AIs", "GOA"),
        bty = "n", horiz = TRUE)
 dev.off()

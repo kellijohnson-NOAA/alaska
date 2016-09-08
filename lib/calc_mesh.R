df33d19be89e60366f124c728e3885a06f1dd7ef
kellijohnson <kellifayejohnson@gmail.com>
2016-09-08 10:38:09 -0700

    Change calc_mesh to two types
    The first type, basic, will create a mesh defined by inla, whereas
    the second type, advanced, will create a mesh based on what I thought
    was the best way to construct the mesh for the case study.

--- lib/calc_mesh.R
+++ lib/calc_mesh.R
@@ -1,10 +1,21 @@
-calc_mesh <- function(locations, boundary = prdomain) {
-  mesh <- inla.mesh.2d(loc = locations,
-    offset = c(-0.06, -0.03),
-    cutoff = 100,
-    max.edge = c(100, 2000),
-    boundary = boundary)
+calc_mesh <- function(locations, boundary = prdomain,
+  type = c("basic", "advanced")) {
 
+  type <- match.arg(type, choices = c("basic", "advanced"),
+    several.ok = FALSE)
+
+  # Create the mesh based on the type specified, where the
+  # default is to use the basic mesh.
+  if (type == "basic") {
+    mesh <- inla.mesh.create(locations, boundary = boundary)
+  }
+  if (type == "advanced") {
+    mesh <- inla.mesh.2d(loc = locations,
+      offset = c(-0.06, -0.03),
+      cutoff = 100,
+      max.edge = c(100, 2000),
+      boundary = boundary)
+  }
   spde <- inla.spde2.matern(mesh)
 
   return(list("mesh" = mesh, "spde" = spde))

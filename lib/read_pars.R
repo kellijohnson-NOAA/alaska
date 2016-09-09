73854be00e010d2c05214dbc99350ef7c51bdce4
kellijohnson <kellifayejohnson@gmail.com>
2016-09-09 12:57:00 -0700

    Forgot OM pars are stored in results$input

--- lib/read_pars.R
+++ lib/read_pars.R
@@ -4,10 +4,10 @@
 #'
 read_pars <- function(results) {
   pars <- c(
-    "om_Range" = results$SpatialScale,
-    "om_rho" = results$om_rho,
-    "om_SigmaO" = results$SD_O,
-    "om_SigmaE" = results$SD_O,
+    "om_Range" = results$input$SpatialScale,
+    "om_rho" = results$input$rho,
+    "om_SigmaO" = results$input$SD_O,
+    "om_SigmaE" = results$input$SD_O,
     "em_Range" = results$Range,
     "em_rho" = results$rho,
     "em_SigmaO" = results$SigmaO,

8bb7669f5f182e3caaad2f0217715c7446fb4622
kellijohnson <kellifayejohnson@gmail.com>
2016-10-06 14:11:00 -0700

    simplify and properly document functions in calc_polys

--- lib/calc_polys.R
+++ lib/calc_polys.R
@@ -5,20 +5,18 @@
 #'
 calc_polys <- function(polygon, lines) {
 
-  projection <- proj4string(polygon)
-  projection.lines <- proj4string(lines)
-  if (projection != projection.lines) stop("The projections",
-    projection, projection.lines, "do not match")
+  if (!sp::identicalCRS(polygon, lines)) stop("The projections",
+    projection(polygon), projection(lines), "do not match")
 
   # create a very thin polygon for each intersected line
   blpi <- rgeos::gBuffer(
     rgeos::gIntersection(polygon, lines, byid = TRUE),
     byid = TRUE, width = 0.000001)
-  proj4string(blpi) <- projection
+  projection(blpi) <- CRS(projection(polygon))
   # split polygon with each thin polygon
   dpi <- rgeos::gDifference(polygon, blpi,
     byid = FALSE, drop_lower_td = TRUE)
-  proj4string(dpi) <- projection
+  projection(dpi) <- sp::CRS(projection(polygon))
 
   return(disaggregate(dpi))
 

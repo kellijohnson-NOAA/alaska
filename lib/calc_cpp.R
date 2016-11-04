23ebc61934641793216ee552936a8757bb1a3cfd
kellijohnson <kellifayejohnson@gmail.com>
2016-11-04 04:22:31 -0700

    Use dir.data for cpp file so that i can run the analysis and the simulations without needing to reload and crashing r.

--- lib/calc_cpp.R
+++ lib/calc_cpp.R
@@ -5,23 +5,13 @@ calc_cpp <- function(cpp, loc) {
   #### Unload and load cpp
   ###############################################################################
   # First attempt
-  firsttry <- try(dyn.unload(dynlib(file.path(cpp))), silent = TRUE)
+  firsttry <- try(dyn.unload(dynlib(file.path(loc, cpp))), silent = TRUE)
   # Garbage Collection
   if(is(firsttry, "try-error")) gc(verbose = FALSE)
   # Second attempt
-  secondtry <- try(dyn.unload(dynlib(file.path(cpp))), silent = TRUE)
-
-  # Check if current directory is same as file location
-  if (getwd() != loc) {
-    firsttry <- try(dyn.unload(dynlib(file.path(loc, cpp))), silent = TRUE)
-    if(is(firsttry, "try-error")) gc(verbose = FALSE)
-    secondtry <- try(dyn.unload(dynlib(file.path(loc, cpp))), silent = TRUE)
-    # Copy the file over to the current directory
-    ignore <- file.copy(file.path(loc, paste0(cpp, ".cpp")),
-      paste0(cpp, ".cpp"))
-  }
+  secondtry <- try(dyn.unload(dynlib(file.path(loc, cpp))), silent = TRUE)
 
   # Compile
-  compile(paste0(cpp, ".cpp"))
-  dyn.load(dynlib(cpp))
+  compile(file.path(loc, paste0(cpp, ".cpp")))
+  dyn.load(dynlib(file.path(loc, cpp)))
 }

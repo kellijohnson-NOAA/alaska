f65516a89a51dec7fa490c8775ea177d0f58f645
kellijohnson <kellifayejohnson@gmail.com>
2016-11-18 07:06:20 -0800

    Add documentation and a check to calc_cpp

--- lib/calc_cpp.R
+++ lib/calc_cpp.R
@@ -1,17 +1,26 @@
+#' Load a \code{\link{TMB}} library.
+#'
+#' The function first checks to see if the library is loaded and then
+#' if so, it removes the library prior to attempting to compile and load it.
+#'
 #' @param cpp A character entry giving the name of the \code{.cpp} file
 #' @param loc The directory location where \code{cpp} is located
 calc_cpp <- function(cpp, loc) {
   ###############################################################################
   #### Unload and load cpp
   ###############################################################################
+  # Make sure that the cpp character value does not have the extension
+  if(grepl("\\.[[:alpha:]]{3}", cpp)) {
+    cpp <- substr(cpp, 1, nchar(basename(cpp)) - 4)
+  }
   # First attempt
-  firsttry <- try(dyn.unload(dynlib(file.path(loc, cpp))), silent = TRUE)
+  firsttry <- try(dyn.unload(TMB::dynlib(file.path(loc, cpp))), silent = TRUE)
   # Garbage Collection
   if(is(firsttry, "try-error")) gc(verbose = FALSE)
   # Second attempt
-  secondtry <- try(dyn.unload(dynlib(file.path(loc, cpp))), silent = TRUE)
+  secondtry <- try(dyn.unload(TMB::dynlib(file.path(loc, cpp))), silent = TRUE)
 
   # Compile
-  compile(file.path(loc, paste0(cpp, ".cpp")))
-  dyn.load(dynlib(file.path(loc, cpp)))
+  TMB::compile(file.path(loc, paste0(cpp, ".cpp")))
+  dyn.load(TMB::dynlib(file.path(loc, cpp)))
 }

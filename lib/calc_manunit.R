55d3c190121001f4cdbc71a44be0f589473c801a
kellijohnson <kellifayejohnson@gmail.com>
2016-12-06 14:36:26 -0800

    manunit is a mess, needs work and add Ono et al.

--- lib/calc_manunit.R
+++ lib/calc_manunit.R
@@ -1,18 +1,17 @@
 #' Calculate management units from a \code{readin} read in using
-#' \code{read_results}.
+#' \code{\link{read_results}}.
 #'
 #' @param readin A list of information read in from the disk using
 #' \code\link{read_results} that summarizes the data used to simulate the
 #' population and the model results.
 #' @param dir The directory to save the files to. The default is \code{getwd()},
-#' or your current working directory.
+#' i.e., your current working directory.
 #' @param file A character value giving the filename to save the resulting
 #' files to. No extension is necessary, as multiple files will be saved with
 #' various extensions using the same name.
 #' @param exe A full file path specifying the location of the SaTScan executible.
-#' The default is \code{NULL}, causing \code{calc_manunit} to use
-#' the \code{\link[SPODT]{spodt}}. Must use backslahes, such that the call to
-#' \code{system} will work. For example: \code{"c:\\SaTScan\\SatScanBatch.exe"}.
+#' Must use backslahes, such that the call to \code{system} will work.
+#' For example: \code{"c:\\SaTScan\\SatScanBatch.exe"}, if on Windows.
 #' @param prmtemplate A full file path specifying the location of the prm template
 #' file to alter for this analysis.
 #' @param projection The spatial projection of the results. The default is
@@ -30,7 +29,7 @@
 #' # Only works on local machine
 #' # prm <- "d:/alaska/data/normal.prm"
 #' # exe <- "c:\\SaTScan\\SaTScanBatch.exe"
-#' # temp <- read_results(data = sim_data, report = Report)
+#' # temp <- read_results(report = Report)
 #' # manunit <- calc_manunit(readin = temp, dir = getwd(),
 #' #   file = "try", exe = exe, prmtemplate = prm)
 #' # rm(exe, manunit, prm, temp)
@@ -39,8 +38,6 @@ calc_manunit <- function(readin, dir = getwd(), file = NULL,
   exe = NULL, prmtemplate = NULL,
   projection = sp::CRS("+proj=longlat +ellps=WGS84"),
   criticalvalue = 0.05, ...) {
-  type <- "satscan"
-  if (is.null(exe)) type <- "spodt"
 
   #1. Pull out the relevant portions of readin
   # info is a subset of the model results for Omega,
@@ -60,68 +57,52 @@ calc_manunit <- function(readin, dir = getwd(), file = NULL,
   }
   # Add a small amount of space to the polygon and then determine which
   # true group each point is in.
+  smallbuffer <- sqrt(rgeos::gArea(bounds$poly)) * 0.0001
   info@data$true <- sp::over(info,
-    rgeos::gBuffer(pol.true, width = 1, byid = TRUE))
+    rgeos::gBuffer(pol.true, width = smallbuffer, byid = TRUE))
 
   #3. Estimate the management units based on spatially-explicit results
-  if (type == "spodt") {
-    cluster.choose <- SPODT::spodt(omega ~ 1, data = info,
-      level.max = NROW(readin$mesh$loc),
-      min.parent = 4, min.child = 2,
-      graft = readin$SigmaO, rtwo.min = 0.001)
-    # Create the lines for each management unit based on the chosen clusters
-    lines.choose <- SPODT::spodtSpatialLines(cluster.choose, data = info)
+  export_prm(file_in = prmtemplate, dir = dir, file = file, ...)
+  export_geo(points = info, dir = dir, file = file,
+    projection = projection)
+  # Double check the direction of slashes and call system
+  system(paste(
+    gsub("/", "\\\\", exe),
+    file.path(gsub("/", "\\\\", dir), paste0(file, ".prm"), fsep = "\\")
+  ), show.output.on.console = FALSE)
+  # Read in the results
+  textfile <- read_txt(dir = dir, file = file)
+  # Get p values
+  if (is.data.frame(textfile$coordinates) | is.matrix(textfile$coordinates)) {
+    pvalues <- as.numeric(sapply(strsplit(
+      textfile$coordinates[grep("P-", textfile$coordinates[, 1]), ],
+      ":"), "[[", 2))
   }
-  if (type == "satscan") {
-    export_prm(file_in = prmtemplate, dir = dir, file = file, ...)
-    export_geo(points = info, dir = dir, file = file,
-      projection = projection)
-    # Double check the direction of slashes and call system
-    system(paste(
-      gsub("/", "\\\\", exe),
-      file.path(gsub("/", "\\\\", dir), paste0(file, ".prm"), fsep = "\\")
-    ), show.output.on.console = FALSE)
-    # Read in the results
-    textfile <- read_txt(dir = dir, file = file)
-    # Get p values
-    if (is.data.frame(textfile$coordinates) | is.matrix(textfile$coordinates)) {
-      pvalues <- as.numeric(sapply(strsplit(
-        textfile$coordinates[grep("P-", textfile$coordinates[, 1]), ],
-        ":"), "[[", 2))
-    }
-    # If the shapefile exists, read it in.
-    shapefile <- file.path(dir, paste0(file, ".col"))
-  }
+  # If the shapefile exists, read it in.
+  shapefile <- file.path(dir, paste0(file, ".col"))
+
 
   # split the full polygon by the estimated polygons
-  if (exists("lines.choose")) {
-    if (length(unique(cluster.choose@partition)) == 1) {
-      pol.choose <- bounds$hull
-    } else {
-      pol.choose <- calc_polys(rgeos::gBuffer(bounds$hull, width = 1),
-        lines.choose)
-    }
-  } else {
-      pol.choose <- SpatialPolygons(
-        lapply(textfile$cluster,
-        function(x, data = info@coords) {
-          X <- data[x, ]
-          X.chull <- chull(X)
-          X.chull <- c(X.chull, X.chull[1])
-          Polygons(list(Polygon(X[X.chull, ])), parent.frame()$i[])
-      }))
-    projection(pol.choose) <- raster::projection(info, asText = FALSE)
-    if (file.exists(paste0(shapefile, ".shp"))) {
-      pol.hull <- readShapePoly(shapefile)
-      pol.hull <- SpatialPolygons(pol.hull@polygons)
-      projection(pol.hull) <- raster::projection("+proj=longlat +ellps=WGS84")
-      if (!exists("pol.choose")) pol.choose <- pol.hull
-    }
-    if (exists("pvalues")) {
-      pol.choose <- pol.choose[pvalues <= criticalvalue]
-    }
+  pol.choose <- sp::SpatialPolygons(
+    lapply(textfile$cluster,
+    function(x, data = info@coords) {
+      X <- data[x, ]
+      X.chull <- chull(X)
+      X.chull <- c(X.chull, X.chull[1])
+      sp::Polygons(list(sp::Polygon(X[X.chull, ])), parent.frame()$i[])
+  }))
+  raster::projection(pol.choose) <- raster::projection(info, asText = FALSE)
+  if (file.exists(paste0(shapefile, ".shp"))) {
+    pol.hull <- maptools::readShapePoly(shapefile)
+    pol.hull <- sp::SpatialPolygons(pol.hull@polygons)
+    raster::projection(pol.hull) <-
+      raster::projection("+proj=longlat +ellps=WGS84")
+    if (!exists("pol.choose")) pol.choose <- pol.hull
   }
-
+  if (exists("pvalues")) {
+    pol.choose <- pol.choose[pvalues <= criticalvalue]
+  }
+browser()
   #4. Calculate the number of points from each true group encompassed
   # in each calculated management unit
   if (length(pol.choose) > 0) {
@@ -197,7 +178,11 @@ calc_manunit <- function(readin, dir = getwd(), file = NULL,
       "Var1" = NA, "Freq" = match.table, "true" = sum(match.table))
   } # End if (is.list(match.table))
   matches$replicate <- readin$replicate
-  matches$scenario <- gsub("sim_|\\.RData", "", basename(readin$file))
+  if(!is.null(file)) {
+    matches$scenario <- file
+  } else {
+    matches$scenario <- gsub("sim_|\\.RData", "", basename(readin$file))
+  }
   matches$subpopulations <- strsplit(matches$scenario, "-")[[1]][1]
   matches$alpha <- strsplit(matches$scenario, "-")[[1]][3]
   matches$Freq[is.na(matches$Freq)] <- 0
@@ -214,14 +199,7 @@ calc_manunit <- function(readin, dir = getwd(), file = NULL,
       pch = ifelse(info$omega >= 0, 2, 6),
       ylab = "northing",
       col = rgb(0, 0, 0, 0.4))
-    if (type == "spodt"){
-      for (ii in 1:length(pol.choose)) {
-        lines(pol.choose[ii], lty = ii + 1, lwd = 2, xpd = TRUE)
-      }
-    }
-    if (type == "satscan") {
-      plot(pol.choose, add = TRUE, lwd = 2, xpd = TRUE)
-    }
+    plot(pol.choose, add = TRUE, lwd = 2, xpd = TRUE)
     lines(readin$lines_grouptrue, lty = 1, lwd = 0.5)
     axis(1, pos = 0)
     text(x = mean(par("usr")[1:2]), y = -350, label = "easting")
